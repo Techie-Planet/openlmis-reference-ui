de1dc30524d06e2be2272440d3304b958247e9eb
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2020-05-22 14:02:15 +0200

    COV-57: Updated usage of COVID_ENV_DIRECTORY param

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,6 @@
 if [ $COVID_ENV_DIRECTORY ]; then
   echo "building image for production instance"
-  cp ./credentials/{$COVID_ENV_DIRECTORY}/.env .env
-else
-  echo "building image for reference instance"
-  cp ./credentials/covid-ref/.env .env
+  cp ./credentials/$COVID_ENV_DIRECTORY/.env .env
 fi
 
 /usr/local/bin/docker-compose pull
@@ -12,14 +9,14 @@ fi
 /usr/local/bin/docker-compose build image
 /usr/local/bin/docker-compose down --volumes
 
-if [ $COVID_ENV_DIRECTORY ]; then
-  echo "pushing image for production instance"
-  docker tag openlmis/covid-ui:latest openlmis/covid-production-ui:${version}
-  docker push openlmis/covid-production-ui:${version}
-else
+if [ $COVID_ENV_DIRECTORY == 'covid-ref' ]; then
   echo "pushing image for reference instance"
   docker tag openlmis/covid-ui:latest openlmis/covid-ui:${version}
   docker push openlmis/covid-ui:${version}
+else
+  echo "pushing image for production instance"
+  docker tag openlmis/covid-ui:latest openlmis/${COVID_ENV_DIRECTORY}-ui:${version}
+  docker push openlmis/${COVID_ENV_DIRECTORY}-ui:${version}
 fi
 
 rm -Rf ./credentials
