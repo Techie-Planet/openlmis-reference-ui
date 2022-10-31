7669906eb953e7f297be699a8742dcbef5422346
Asquarep <asquarep@yahoo.com>
2022-10-31 22:51:36 +0100

    issue summary debug

--- src/stock-adjustment-creation/adjustment-creation.service.js
+++ src/stock-adjustment-creation/adjustment-creation.service.js
@@ -103,8 +103,8 @@
             return repository.create(event)
                 .then(function(response) {
                     $rootScope.$emit('openlmis-referencedata.offline-events-indicator');
-                    return response;
-                    // return Object.values(response.value).join("");
+                    // return response;
+                    return Object.values(response[0][0]).join("");
                 });
         }
 
