a038aa0abb8ef675767a665abd3f9bca1031c64c
Asquarep <asquarep@yahoo.com>
2023-04-06 07:52:31 +0100

    vvm status issue page testing

--- src/stock-adjustment-creation/adjustment-creation.controller.js
+++ src/stock-adjustment-creation/adjustment-creation.controller.js
@@ -318,6 +318,7 @@
          * @param {Object} lineItem line item to be validated.
          */
         vm.validateVVMStatus = function(lineItem) {
+            console.log(lineitem);
             if (lineItem.orderable.extraData.useVVM === 'true') {
                 lineItem.$errors.vvmStatusInvalid = isEmpty(lineItem.vvmStatus);
             }
