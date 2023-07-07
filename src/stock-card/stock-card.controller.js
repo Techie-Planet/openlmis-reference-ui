a09454eab3c83e567d65c002c61ea3c7f6bdf249
Asquarep <asquarep@yahoo.com>
2023-07-07 18:28:12 +0100

    sublots feature phase 2

--- src/stock-card/stock-card.controller.js
+++ src/stock-card/stock-card.controller.js
@@ -87,15 +87,11 @@
          *
          */
         function sublotChanged(){
-            console.log("sublot changed. sublot selected is: ", vm.selectedSublot);
             if (vm.selectedSublot) {
-                console.log("getting sublot stock card");
                 loadingModalService.open();
                 vm.sublotStockCard = stockCardService.getSublotStockCard(vm.selectedSublot)
                 .then(function(sublotStockCard) {
-                    console.log("got the card", sublotStockCard);
                     vm.sublotStockCard = sublotStockCard;
-                    console.log(vm.sublotStockCard);
                     //vm.displayedLineItems = sublotStockCard.sublotLineItems;
                 })
                 
@@ -187,17 +183,6 @@
                 return 'I' + convertToRoman(num - 1);
             }
         }
-
-        // $scope.$watch(function() {
-        //     return vm.sublotstockcard;
-        //   }, function(newValue) {
-        //     if (newValue && newValue.sublotLineItems) {
-        //       $state.go($state.current.name, $stateParams, {
-        //         reload: false,
-        //         notify: false
-        //       });
-        //     }
-        //   }, true);
 
         function onInit() {
             $state.current.label = stockCard.orderable.fullProductName;
